55a6f94fe9de1b96e32ded42eb3a6efa7507bf11
SzaszArnold <arnoldszasz06@gmail.com>
2020-11-30 20:00:59 +0200

    Try retrofit callback

--- AndroidProject/app/src/main/java/com/android/service/androidproject/ui/home/HomeFragment.kt
+++ AndroidProject/app/src/main/java/com/android/service/androidproject/ui/home/HomeFragment.kt
@@ -5,10 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.android.service.androidproject.API.herokuAPI
 import com.android.service.androidproject.R
+import org.json.JSONArray
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class HomeFragment : Fragment() {
 
@@ -25,6 +32,19 @@ class HomeFragment : Fragment() {
         val textView: TextView = root.findViewById(R.id.text_home)
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
+        })
+        herokuAPI.endpoints.getCountries().enqueue(object : Callback<JSONArray> {
+            override fun onResponse(call: Call<JSONArray>, response: Response<JSONArray>) {
+                if (response.isSuccessful) {
+
+                    Toast.makeText(context, "Success", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show()
+                }
+            }
+            override fun onFailure(call: Call<JSONArray>, t: Throwable) {
+                Toast.makeText(context, "Failed-onFailure", Toast.LENGTH_SHORT).show()
+            }
         })
         return root
     }
